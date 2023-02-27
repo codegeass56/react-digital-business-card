a54bf6f5b5039dfb32ef545f63e38a19b3008168
Siddharth Panjwani <56246862+codegeass56@users.noreply.github.com>
2023-02-27 20:01:01 +0400

    Create initial project structure

---
+++ src/components/Info.jsx
@@ -0,0 +1,5 @@
+function Info() {
+  return <h1>This is the Info section</h1>;
+}
+
+export default Info;
---
+++ src/components/Interests.jsx
@@ -0,0 +1,5 @@
+function Interests() {
+  return <h1>This is the Interest section</h1>;
+}
+
+export default Interests;
